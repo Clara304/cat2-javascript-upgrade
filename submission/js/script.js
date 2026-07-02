edf27c88ee9aa75d0813115b80bb72fbe9d4a59e
Clara304 <marieclaire.bazibuhe@strathmore.edu>
2026-07-02 23:06:20 +0300

    Add dynamic content with loop

---
+++ submission/js/script.js
@@ -0,0 +1,44 @@
+// Grilled meats menu
+
+const grilledMeats = [
+    {
+        name: "Brochette Boeuf",
+        price: "KSh 1,500"
+    },
+    {
+        name: "Brochette Chèvre",
+        price: "KSh 1,700"
+    },
+    {
+        name: "Porc Choma",
+        price: "KSh 2,000"
+    },
+    {
+        name: "Ntaba Choma",
+        price: "KSh 2,200"
+    },
+    {
+        name: "Chicken Choma",
+        price: "KSh 1,800"
+    }
+];
+
+const grilledMenu = document.getElementById("grilledMenu");
+
+grilledMeats.forEach(function(item) {
+
+    const menuItem = document.createElement("div");
+    menuItem.className = "menu-item";
+
+    const foodName = document.createElement("span");
+    foodName.textContent = item.name;
+
+    const foodPrice = document.createElement("span");
+    foodPrice.textContent = item.price;
+
+    menuItem.appendChild(foodName);
+    menuItem.appendChild(foodPrice);
+
+    grilledMenu.appendChild(menuItem);
+
+});
